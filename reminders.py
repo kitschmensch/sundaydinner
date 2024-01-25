6146115541971e0ed3e2e0cdc1221241c6fc5629
Jake Mecham <jake@mecham.xyz>
2024-01-25 17:01:16 -0600

    Fixed

--- reminders.py
+++ reminders.py
@@ -18,9 +18,8 @@ with open("config.yml", "r") as config_file:
     config = yaml.safe_load(config_file)
 
 # Config
-API_KEY = config["API_KEY"]
 LOGGING_LEVEL = config["LOGGING_LEVEL"]
-SCOPES = config["SCOPES"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 SPREADSHEET_ID = config["SPREADSHEET_ID"]
 EVENTS = config["EVENTS"]
 MEMBERS = config["MEMBERS"]
@@ -41,10 +40,27 @@ def main():
         format="%(asctime)s %(message)s",
         datefmt="%m/%d/%Y %I:%M:%S %p",
     )
+    creds = None
+
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+        creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
 
     logging.info("Credentials valid. Fetching data from Google Sheets")
     try:
-        service = build("sheets", "v4", developerKey=API_KEY)
+        service = build("sheets", "v4", credentials=credentials, cache_discovery=False)
         sheet = service.spreadsheets()
         eventResult = (
             sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=EVENTS).execute()
